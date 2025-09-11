c2c11248b87bd4d76c11af06c621c88fe1221f07
lovepreet857 <singhlovepreet8575@gmail.com>
2025-09-11 11:12:12 +0530

    hjfhh

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -46,7 +46,7 @@ const Projects = () => {
       </div>
 
       {/* Grid */}
-      <div className="hidden lg:grid grid-cols-2 xl:grid-cols-3 gap-10  md:my-20">
+      <div className="hidden lg:grid grid-cols-2 xl:grid-cols-3 gap-10  md:mt-20 md:mb-5">
         {projectdata.map((item, index) => (
           <motion.div
             key={index}
